fd041b0d8142f3923c405a7d5cf864994ddaff84
50Storm <hirotech.net@gmail.com>
2019-05-03 17:30:12 +0900

    modified code

--- application.py
+++ application.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-# chat_data = {"room": "", "message": "" }
-chat_data={}
+chat_data={} # chat_data = {"room": "", "message": "" }
 room="";
 
 @app.route("/")
@@ -29,55 +28,44 @@ def on_join(data):
     display_name = data['displayName']
     room = data['room']
     now = datetime.datetime.now()
-    str_now = now.strftime("%Y/%m/%d %H:%M:%S")
-    
+    str_now = now.strftime("%Y/%m/%d %H:%M:%S")    
     print("room => " + str(room))
     join_room(room)
     msg =  str(display_name)  + ' has entered ' + room + " " + str_now
 
-    
-       
     if not room in chat_data.keys() :
         chat_data[room] = ""
-    
-    # for k in chat_data:
-        # print("key =>" + k)
-        # print("chat_data =>" + chat_data[k])
-
-    # room_message = chat_data[room]
-    # print("room_message => "  +  room_message)
-    #  msg=<span class="text-info"></span> has entered room01  chat_data={room: "room01", message: "test"}
     emit("join room message from server", (msg, chat_data, room), room=room)
-
- 
-# @socketio.on('show rooms')
-# def on_show_rooms(data):
-    # msg = data['diplayName'] + "'" +  "room list : ".join(rooms())
-    # emit("join room message from server", (msg, chat_data, room))
 
 @socketio.on("send message")
 def on_message(data):
     print("==================================== on_message ========================================= ")
     print(data)
-    now = datetime.datetime.now()
-    str_now = now.strftime("%Y/%m/%d %H:%M:%S")
+    #dataから取り出す
     room = data['room']
     displayName = data['displayName']
-    message = displayName + " " + data["message"]  + " " + str_now
+    message_from_client = data["message"]
     
+    #サーバーの時刻を追加
+    now = datetime.datetime.now()
+    str_now = now.strftime("%Y/%m/%d %H:%M:%S")
+
+    #メッセージを作る
+    message = displayName + " " + str(message_from_client)  + " " + str_now
+
+    #メッセージがN件ですか？
     
+    #メッセージを追加
     if room in chat_data.keys() :
+        #<br/>でメッセージ追加
         chat_data[room] = chat_data[room] + message + "<br/>"
     else:
+        #初めてのメッセージ
         chat_data[room] = message + "<br/>"
 
     print("chat_data[room] " + chat_data[room])
-    
-    
     # print("message " + message)
     emit("message from server", message, room=room)
-    
-    
 
  # if __name__ == '__main__':
  #    socketio.run(app)
